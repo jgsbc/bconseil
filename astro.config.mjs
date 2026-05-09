598d4d294bfa31aa127c10155a13a9db0d147d98
lejaume <jgcencig@gmail.com>
2026-05-09 02:42:06 +0200

    Version dev V0.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ const isGitHubActions = process.env.GITHUB_ACTIONS === 'true';
 // https://astro.build/config
 export default defineConfig({
   site: isGitHubActions ? 'https://jgsbc.github.io' : 'https://www.berliozconseil.fr',
-  base: isGitHubActions ? '/bconseil' : '/',
+  base: isGitHubActions ? '/bconseil/' : '/',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
